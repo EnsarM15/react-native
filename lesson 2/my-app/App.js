eeda0dd447778050a5f929460b1bd8298a810680
EnsarM15 <165320780+EnsarM15@users.noreply.github.com>
2025-11-27 20:47:32 +0100

    ..

--- lesson 2/my-app/App.js
+++ lesson 2/my-app/App.js
@@ -5,30 +5,32 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import reserveScreen from "./screens/reserveScreen";
 
+
 import MainScreen from "./screens/MainScreen";
 
 import ListScreen from "./screens/ListScreen"; // 👈 like ListScreen in the example
-
+import StudentDetail from "./components/StudentDetail";
+import StudentScreen from "./screens/StudentScreen";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-     <NavigationContainer>
-     <Stack.Navigator
-        initialRouteName="Main"
-        screenOptions={{
-         title: "App",
-        }}
-      >
-        <Stack.Screen name="Main" component={MainScreen} />
+   //  <NavigationContainer>
+    // <Stack.Navigator
+    //    initialRouteName="Main"
+  //      screenOptions={{
+  //       title: "App",
+   //     }}
+   //   >
+    //    <Stack.Screen name="Main" component={MainScreen} />
 
-         <Stack.Screen name="List" component={ListScreen} />
-          <Stack.Screen name="reserve" component={reserveScreen} />
-      </Stack.Navigator>
+      //   <Stack.Screen name="List" component={ListScreen} />
+      //    <Stack.Screen name="reserve" component={reserveScreen} />
+  //    </Stack.Navigator>
 
-      <StatusBar style="auto" />
-     </NavigationContainer>
- 
+     // <StatusBar style="auto" />
+   //  </NavigationContainer>
+    <StudentScreen></StudentScreen>
   );
 }
 
